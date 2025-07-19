ee2569c02571e48c0712d2b5a07d4e7df7fe6b8b
asilbekali <asilbekabdugafforov7@gmail.com>
2025-07-19 16:23:19 +0500

    asab bizar corsi togirlanid

--- intex_backend/src/main.ts
+++ intex_backend/src/main.ts
@@ -1,4 +1,3 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -10,22 +9,16 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: function (origin, callback) {
-      const allowedOrigins = ['http://localhost:5173'];
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: ['http://localhost:5173'],
     credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
   const config = new DocumentBuilder()
     .setTitle('INDEX example')
     .setDescription('The INDEX API description')
     .setVersion('1.0')
-    .addSecurityRequirements('bearer', ['bearer'])
     .addBearerAuth()
     .addTag('INDEX')
     .build();

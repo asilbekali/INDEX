e0635fa86898bc28649b53f1d569df74d5167fbd
asilbekali <asilbekabdugafforov7@gmail.com>
2025-07-21 14:55:30 +0500

    consultation crud finished

---
+++ intex_backend/dist/consultation/dto/create-consultation.dto.d.ts
@@ -0,0 +1,4 @@
+export declare class CreateConsultationDto {
+    name: string;
+    phone: string;
+}
--- intex_backend/src/consultation/consultation.module.ts
+++ intex_backend/src/consultation/consultation.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConsultationService } from './consultation.service';
 import { ConsultationController } from './consultation.controller';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ConsultationController],
   providers: [ConsultationService],
 })
